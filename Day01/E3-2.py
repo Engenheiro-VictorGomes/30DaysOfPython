ca27d5d45e82c62862cdb935f68ffd901696c0ae
Engenheiro-VictorGomes <vhdasg@gmail.com>
2023-03-07 12:03:27 +0100

    Day1-Done

---
+++ Day01/E3-2.py
@@ -0,0 +1,7 @@
+# Find an Euclidian distance between (2, 3) and (10, 8)
+
+point1 = (2,3)
+point2 = (10,8)
+
+EuclidianDistance = ((point2[0]-point1[0])**2 + (point2[1]-point1[1])**2)**(1/2)
+print(EuclidianDistance)
