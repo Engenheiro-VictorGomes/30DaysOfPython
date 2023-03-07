562416043afcd3b6ce5a1ebdfd5a0f66e34cc16b
Engenheiro-VictorGomes <vhdasg@gmail.com>
2023-03-07 12:04:25 +0100

    new file:   Day01/E3-1.py 	new file:   Day01/E3-2.py

---
+++ Day01/E3-1.py
@@ -0,0 +1,15 @@
+# Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+vInteger = 1
+vFloat = 1.0
+vComplex = 1.1 + 1.2j
+vString = "String"
+vBoolean = True
+vList = [1,2,3,4,5]
+vTuple = ('Earth', 'Jupiter', 'Neptune', 'Mars', 'Venus', 'Saturn', 'Uranus', 'Mercury')
+vSet = {3.14, 9.81, 2.7}
+vDictionary = {
+    'Name':'square_1',
+    'Color':'blue',
+    'xSize':5,
+    'ySize':5,
+}
