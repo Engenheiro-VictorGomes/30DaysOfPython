3573add65884d142c0406b075d32ec42116903fc
Engenheiro-VictorGomes <vhdasg@gmail.com>
2023-03-13 16:09:37 +0100

    new file:   Day16/E1.py 	modified:   FilesGenerator.py 	modified:   List.txt

---
+++ Day16/E1.py
@@ -0,0 +1,9 @@
+# 1. Get the current day, month, year, hour, minute and timestamp from datetime module
+from datetime import datetime
+now = datetime.now()
+day = now.day
+month = now.month
+year = now.year
+hour = now.hour
+minute = now.minute
+timestamp = now.timestamp()
