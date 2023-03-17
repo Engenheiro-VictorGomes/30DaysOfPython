980280cf3a6df750a796154e58ae2fc67a4574ea
Engenheiro-VictorGomes <vhdasg@gmail.com>
2023-03-17 15:27:44 +0100

    modified:   Day19/E8.py 	modified:   Day19/E9.py 	new file:   Day19/E9_Performance

--- Day19/E8.py
+++ Day19/E8.py
@@ -1,1 +1,62 @@
 # 8. Find the 10 most repeated words in the romeo_and_juliet.txt
+import re
+
+
+def find_most_common_words(filename = None, n=10, remove_words = None):
+    # Check Parameters
+    if n <= 0:
+        raise ValueError
+    if filename is None:
+        raise ValueError
+    if remove_words is None:
+        remove_words = []
+
+    # Open file and extract words
+    with open(filename) as file:
+        words = []
+        for line in file:
+            words.extend(re.findall(r'\b\w+\b', line))
+    words = list(map(lambda x: x.lower(), words))
+    
+    # Remove words
+    for remove_word in remove_words:
+        words = list(filter(lambda x: x != remove_word, words))
+
+    # Get words counting
+    words_count = {}
+    for word in words:
+        if word in words_count:
+            words_count[word] += 1
+        else:
+            words_count[word] = 1
+    return(sorted([(count, word) for word, count in words_count.items()], reverse=True)[:n])
+
+remove_words = ['the', 'and', 'to', 'of', 'a', 'an', 'in', 'with', 'that', 'this', 's', 'd']
+print(find_most_common_words(filename='./data/romeo_and_juliet.txt', n=10, remove_words=remove_words))
+
+# Solution 02:
+def find_most_common_words_Solution02(filename = None, n=10, remove_words = None):
+    # Check Parameters
+    if n <= 0 or filename is None:
+        raise ValueError
+    if remove_words is None:
+        remove_words = []
+
+    # Open file and extract words
+    with open(filename) as file:
+        words = re.findall(r'\b\w+\b', file.read().lower())
+    
+    # Remove words
+    remove_words = set(remove_words) if remove_words else set()
+    words = [word for word in words if word not in remove_words and re.match('^[a-z]+$', word)]
+
+    # Get words counting
+    words_count = {}
+    for word in words:
+        if word in words_count:
+            words_count[word] += 1
+        else:
+            words_count[word] = 1
+    return(sorted([(count, word) for word, count in words_count.items()], reverse=True)[:n])
+
+print(find_most_common_words_Solution02(filename='./data/romeo_and_juliet.txt', n=10, remove_words=remove_words))
--- Day19/E9.py
+++ Day19/E9.py
@@ -1,4 +1,23 @@
 # 9. Read the hacker news csv file and find out:
 #     a) Count the number of lines containing python or Python
 #     b) Count the number lines containing JavaScript, javascript or Javascript
-#     c) Count the number lines containing Java and not JavaScript
+#     c) Count the number lines containing C and not JavaScript
+import re
+
+
+counter_python = 0
+counter_javascript = 0
+counter_java = 0
+
+with open("./data/hacker_news.csv") as file:
+    for line in file:
+        if re.search(r'\bpython\b|\bPython\b',line):
+            counter_python+=1
+        if re.search(r'\bJavaScript\b|\bjavascript\b|\bJavascript\b',line):
+            counter_javascript+=1
+        if re.search(r'\bJava\b|\bjava\b',line):
+            counter_java+=1
+
+print(f"Python = {counter_python}")
+print(f"Javascript = {counter_javascript}")
+print(f"Java = {counter_java}")
